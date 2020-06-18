9708f49053353dfe52732590a6ca5ce397e2e00c
AlgrisMarquez <algrismarquez16@gmail.com>
2020-06-17 20:34:22 -0400

    hola

--- src/index.js
+++ src/index.js
@@ -14,45 +14,45 @@ document.getElementById('go').addEventListener('click', () =>{
 document.getElementById('validation').addEventListener('click', () =>{
     //condiciona que los inputs no pueden estar vacios 
       if (document.getElementById('userName').value !=='' && document.getElementById('creditCard').value !=='' ){
-    //captura nombre del usuario
-      const name= document.getElementById('userName').value;
-    //captura numero de tarjeta
-      creditcardNumber=document.getElementById('creditCard').value;
-    //oculta pantalla de ingreso de datos
-      document.getElementById('inputCreditCard').style.display="none";
-    //muestra pantalla de resultados de la tarjeta
-      document.getElementById('valid').style.display="block";
-  //convertir el nro de tarjeta en un array
-    let cardNumberA=[];
-    for (let i=0; i<creditcardNumber.length; i++){
-     cardNumberA.push(parseInt(creditcardNumber[i]));
-    }
-  //luego de validar
-    let valid=validator.isValid(cardNumberA);
-    if (valid===true){
-  //llamando a maskify
-    let newCreditCardNumber=validator.maskify(creditcardNumber);
-  //mostrando resultados en pantalla valid
-    document.getElementById('messageValid').innerHTML='Que bien, ' + name.toUpperCase();
-    document.getElementById('imageCheck').innerHTML='<img src="img/check.png" alt="check" class="imgcheck">'
-    document.getElementById('message').innerHTML='Aqui te mostramos los beneficios a los que puedes acceder con tu tarjeta'
-    document.getElementById('cardNumberAfter').innerHTML= 'Numero '+ newCreditCardNumber;
-    document.getElementById('benefits').innerHTML='<div class="hyperlink"><a href="https://www.levi.cl/" class="text" target="blank"> 30% off zapatillas en Levis</a></div><br>'+
+      //captura nombre del usuario
+       const name= document.getElementById('userName').value;
+      //captura numero de tarjeta   
+       creditcardNumber=document.getElementById('creditCard').value;
+      //oculta pantalla de ingreso de datos
+        document.getElementById('inputCreditCard').style.display="none";
+      //muestra pantalla de resultados de la tarjeta
+        document.getElementById('valid').style.display="block";
+      //convertir el nro de tarjeta en un array
+      let cardNumberA=[];
+      for (let i=0; i<creditcardNumber.length; i++){
+        cardNumberA.push(parseInt(creditcardNumber[i]));
+      }
+      //luego de validar
+      let valid=validator.isValid(cardNumberA);
+        if (valid===true){
+        //llamando a maskify
+        let newCreditCardNumber=validator.maskify(creditcardNumber);
+          //mostrando resultados en pantalla valid
+          document.getElementById('messageValid').innerHTML='Que bien, ' + name.toUpperCase();
+          document.getElementById('imageCheck').innerHTML='<img src="img/check.png" alt="check" class="imgcheck">'
+          document.getElementById('message').innerHTML='Aqui te mostramos los beneficios a los que puedes acceder con tu tarjeta'
+          document.getElementById('cardNumberAfter').innerHTML= 'Numero '+ newCreditCardNumber;
+          document.getElementById('benefits').innerHTML='<div class="hyperlink"><a href="https://www.levi.cl/" class="text" target="blank"> 30% off zapatillas en Levis</a></div><br>'+
 '<div class="hyperlink"><a href="https://www.lider.cl/" class="text" target="blank">Despacho gratis en Lider.cl solo RM</a></div><br>'+
 '<div class="hyperlink"><a href="https://www.falabella.com/falabella-cl/" class="text" target="blank">35%off moda mujer en Falabella.cl</a></div><br>'
-    }
-    else {
-      document.getElementById('messageValid').innerHTML='Lo sentimos, ' + name.toUpperCase();
-      document.getElementById('message').innerHTML='En estos momentos no encontramos beneficios para tu tarjeta';
-      document.getElementById('cardNumberAfter').innerHTML='Numero '+ newCreditCardNumber;
-    } 
-  }
-  // mostrar mensajes si el input esta vacio al hacer click
-  else{
-    document.getElementById('empty1').innerHTML='El campo no puede estar vacio';
-    document.getElementById('empty2').innerHTML='El campo no puede estar vacio';
-  }
-});
+        }
+        else {
+          document.getElementById('messageValid').innerHTML='Lo sentimos, ' + name.toUpperCase();
+          document.getElementById('message').innerHTML='En estos momentos no encontramos beneficios para tu tarjeta';
+          document.getElementById('cardNumberAfter').innerHTML='Numero '+ newCreditCardNumber;
+        } 
+      }
+      // mostrar mensajes si el input esta vacio al hacer click
+      else{
+        document.getElementById('empty1').innerHTML='El campo no puede estar vacio';
+        document.getElementById('empty2').innerHTML='El campo no puede estar vacio';
+      }
+  });
 
 //funcionalidad del boton Volver (regresa a la pantalla principal)
 document.getElementById('return').addEventListener('click', () =>{
--- src/validator.js
+++ src/validator.js
@@ -1,40 +1,39 @@
-const  validator  =  {
+const validator = {
   
-    isValid ( cardNumberA ) {
-        let  resultCardNumber = 0 ;
-    // duplicar posiciones pares
-        for  ( let  i = 0 ;  i < cardNumberA . length ;  i + = 2 ) {
-            cardNumberA [ i ] = ( cardNumberA [ i ] * 2 ) ;
-        }
-    // verificar que el producto no sea mayor a 9, si es mayor sumar
-        for  ( let  i = 0 ;  i < cardNumberA . length ;  i ++ )  {
-            if  ( cardNumberA [ i ] > 9 ) {
-            let  numero = ( parseInt ( cardNumberA [ i ] / 10 ) ) + ( cardNumberA [ i ] % 10 ) ;
-            cardNumberA [ i ] = numero ;
-            consola . log ( numero ) ;
-        }
+  isValid(cardNumberA){
+      let resultCardNumber=0;
+  //duplicar posiciones pares
+      for (let i=0; i<cardNumberA.length; i+=2){
+          cardNumberA[i]=(cardNumberA[i]*2);
       }
-    // sumando todos los nros de la tarjeta
-        for  ( let  i = 0 ;  i < cardNumberA . length ;  i ++ ) {
-            resultCardNumber + = cardNumberA [ i ] ;
-            consola . log ( resultCardNumber ) ;
-        }
-        if  ( resultCardNumber % 10 === 0 ) {
-          //console.log('valido ');
-            volver  verdadero ;
-        }
-        más {
-          //console.log('invalido ');
-            devuelve  falso ;
-        }
-    } ,
-  
-     maskify (número de tarjeta de crédito )  {
-  
-      // ocultando los primeros digitos de la tarjeta, excepto los utimos 4 "maskify"
-      let  cardNumberMask = número de tarjeta de crédito . rebanada ( 0 , - 4 ) . reemplace ( /./g , '#' ) + número de tarjeta de crédito . rebanada ( - 4 ) ;
-      return  cardNumberMask ;
-     }
+  //verificar que el producto no sea mayor a 9, si es mayor sumar
+      for (let i=0; i<cardNumberA.length; i++) {
+          if (cardNumberA[i]>9){
+          let numero=(parseInt(cardNumberA[i]/10))+(cardNumberA[i]%10);
+          cardNumberA[i]=numero;
+          console.log(numero);
+      }
     }
-     validador predeterminado de  exportación ;
- 
+  //sumando todos los nros de la tarjeta
+      for (let i=0; i<cardNumberA.length; i++){
+          resultCardNumber+=cardNumberA[i];
+          console.log(resultCardNumber);
+      }
+      if (resultCardNumber%10===0){
+        //console.log('valido');
+          return true;
+      }
+      else{
+        //console.log('invalido');
+          return false;
+      }
+  },
+
+   maskify(creditcardNumber) {
+
+    //ocultando los primeros digitos de la tarjeta, exceptuando los utimos 4 "maskify"
+    let cardNumberMask=creditcardNumber.slice(0,-4).replace(/./g,'#')+creditcardNumber.slice(-4);
+    return cardNumberMask;
+   }
+  }
+  export default validator;
